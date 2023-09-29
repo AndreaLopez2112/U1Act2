50f208f4fb289e65ab00278e6a8a7ab3215fc181
Andrea <Andrea@DESKTOP-D0S117B>
2023-09-28 23:27:18 -0600

    Agregar archivos de proyecto.

---
+++ U1Act2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace U1Act2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string Moneda, decimal Cantidad)
+        {
+            if (Moneda == "Dolar")
+            {
+                decimal Resultado = Cantidad / 17;
+                return View(Resultado);
+            }
+            else
+            {
+                decimal Resultado = 17 * Cantidad;
+                return View(Resultado);
+            }
+           
+        }
+    }
+}
